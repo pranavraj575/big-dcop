06f169b0ece09721bebb853d6174f1b59e369164
pranav <prajbhan@cs.cmu.edu>
2026-02-14 10:41:16 -0500

    allow for collecting mid-run data

--- evaluation/graph_coloring_runner.py
+++ evaluation/graph_coloring_runner.py
@@ -4,28 +4,33 @@ import glob
 import argparse
 import pandas as pd
 import json
-import itertools
 
 from eval_helpers import reformat_file_for_maxsum, extract_json_from_output
 from algo_configs import get_algo_info, get_display_name
 
 
-def run_pydcop(problem_file, algo_config, timeout):
+def run_pydcop(problem_file, algo_config, args):
     """
     Runs pydcop solve.
-    Returns a dictionary of metrics.
+    Returns a df(of mid run metrics), dict(of final metrics).
     """
     base_name, extra_alg_params = get_algo_info(algo_config)
 
     # build Command
     cmd = [
-        "pydcop", "--timeout", str(timeout), "solve",
+        "pydcop", "--timeout", str(args.timeout), "solve",
+        problem_file,
         "--algo", base_name,
-        problem_file
     ]
 
     # append the specific args from the dict
     cmd.extend(extra_alg_params)
+
+    if args.collect_on is not None:
+        cmd.extend(['--collect_on', args.collect_on])
+        if args.period is not None:
+            cmd.extend(['--period', str(args.period)])
+        cmd.extend(['--run_metrics', args.temp_csv])
 
     try:
         result = subprocess.run(
@@ -36,37 +41,58 @@ def run_pydcop(problem_file, algo_config, timeout):
         )
 
         # Parse Output
-        data = extract_json_from_output(result.stdout)
-
-        if data and "assignment" in data:
-            return {
+        final_data = extract_json_from_output(result.stdout)
+        if args.collect_on is not None:
+            mid_df = pd.read_csv(args.temp_csv)
+        else:
+            mid_df = None
+        if final_data and "assignment" in final_data:
+            return mid_df, {
                 "status": "1",  # 1 = success
-                "cost": float(data.get("cost", -1)),
-                "time": float(data.get("time", -1)),
-                "msg_count": int(data.get("msg_count", 0)),
-                "cycles": int(data.get("cycles", 0)),
-                "assignment": str(data.get("assignment", {}))  # store as string to fit in DF
+                "cost": float(final_data.get("cost", -1)),
+                "time": float(final_data.get("time", -1)),
+                "msg_count": int(final_data.get("msg_count", 0)),
+                "cycles": int(final_data.get("cycles", 0)),
+                "assignment": str(final_data.get("assignment", {}))  # store as string to fit in DF
             }
         else:
             # failed or timed out
             err_msg = result.stderr.strip() if result.stderr else result.stdout.strip()[-200:]
-            return {"status": "FAILED", "error": err_msg}
+            return mid_df, {"status": "FAILED", "error": err_msg}
 
     except Exception as e:
-        return {"status": "ERROR", "error": str(e)}
+        return None, {"status": "ERROR", "error": str(e)}
 
 
 def main():
+    DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     parser = argparse.ArgumentParser(description="Run pyDCOP benchmark")
-    parser.add_argument("--algorithms", type=str, default="evaluation/algorithm_configs.json",
+    parser.add_argument("--algorithms", type=str, default=os.path.join(DIR, "evaluation", "algorithm_configs.json"),
                         help="json file with algorithm configs to use")
-    parser.add_argument("--input_dir", type=str, default="output/graph_coloring_instances",
+    parser.add_argument("--input_dir", type=str, default=os.path.join(DIR, "output", "graph_coloring_instances"),
                         help="Directory containing .yaml problem files")
-    parser.add_argument("--output_csv", type=str, default="output/results.csv",
+    parser.add_argument("--output_csv", type=str, default=os.path.join(DIR, "output", "results.csv"),
                         help="Path to save the results DataFrame")
+    parser.add_argument("--temp_csv", type=str, default=os.path.join(DIR, "output", "temp.csv"),
+                        help="Path to save mid-run results into (cleared after running each experiemnt)")
     parser.add_argument("--trials", type=int, default=1, help="Number of trials per algorithm per problem")
-    parser.add_argument("--timeout", type=float, default=[10.], nargs='+',
-                        help="Timeout in seconds per run, use multiple values if you want to run with different paramters")
+    parser.add_argument("--timeout", type=float, default=10.,
+                        help="Timeout in seconds per run")
+    parser.add_argument(
+        "-c",
+        "--collect_on",
+        choices=["value_change", "cycle_change", "period"],
+        default=None,
+        help='collect mid-run data upon this event',
+    )
+    parser.add_argument(
+        "--period",
+        type=float,
+        default=None,
+        help="Period for collecting metrics. only available "
+             "when using --collect_on period. Defaults to 1 "
+             "second if not specified",
+    )
     args = parser.parse_args()
 
     if not os.path.exists(args.algorithms):
@@ -85,7 +111,6 @@ def main():
         print(f"No .yaml files found in {args.input_dir}")
         return
 
-    all_records = []
     # loop over problems
     for p_idx, problem_path in enumerate(problem_files):
         problem_name = os.path.basename(problem_path)
@@ -94,48 +119,72 @@ def main():
         # apply fix for maxsum if needed
         reformat_file_for_maxsum(problem_path)
 
-        for algo_config in algorithms:
+        for algo_idx, algo_config in enumerate(algorithms):
             display_name = get_display_name(algo_config)
             print(f"    Algorithm: {display_name}")
-            for timeout, trial in itertools.product(args.timeout, range(args.trials)):
+            for trial in range(args.trials):
+                trial_records = []
                 # Run the solver
-                metrics = run_pydcop(
+                mid_df, final_metrics = run_pydcop(
                     problem_file=problem_path,
                     algo_config=algo_config,
-                    timeout=timeout,
+                    args=args,
                 )
 
-                # Create a record row
-                record = {
-                    "problem": problem_name,
-                    "algorithm": display_name,
-                    "trial": trial + 1,
-                    "status": metrics.get("status"),
-                    "cost": metrics.get("cost"),
-                    "time": metrics.get("time"),
-                    "msg_count": metrics.get("msg_count"),
-                    "cycles": metrics.get("cycles"),
-                    "timeout_param": timeout,
-                    "error_msg": metrics.get("error", "")
-                }
-
-                all_records.append(record)
+                # if we are taking mid-run statistics,
+                #   the last row will include final metrics
+                if mid_df is not None:
+                    for row in mid_df.itertuples():
+                        mid_record = {
+                            "problem": problem_name,
+                            "algorithm": display_name,
+                            "trial": trial + 1,
+                            "status": row.status,
+                            "cost": row.cost,
+                            "time": row.time,
+                            "msg_count": row.msg_count,
+                            "cycles": row.cycle,
+                            "timeout_param": args.timeout,
+                            "error_msg": final_metrics.get("error", "")
+                        }
+                        trial_records.append(mid_record)
+                else:
+                    # Create a record row=
+                    record = {
+                        "problem": problem_name,
+                        "algorithm": display_name,
+                        "trial": trial + 1,
+                        "status": final_metrics.get("status"),
+                        "cost": final_metrics.get("cost"),
+                        "time": final_metrics.get("time"),
+                        "msg_count": final_metrics.get("msg_count"),
+                        "cycles": final_metrics.get("cycles"),
+                        "timeout_param": args.timeout,
+                        "error_msg": final_metrics.get("error", "")
+                    }
+                    trial_records.append(record)
                 print(
-                    f"        Trial {trial + 1}: {metrics.get('status')},"
-                    f" Cost: {metrics.get('cost', 'N/A')},"
-                    f" Time: {metrics.get('time', 'N/A')})")
-                error_msg = metrics.get("error", "")
+                    f"        Trial {trial + 1}: {final_metrics.get('status')},"
+                    f" Cost: {final_metrics.get('cost', 'N/A')},"
+                    f" Time: {final_metrics.get('time', 'N/A')})")
+                error_msg = final_metrics.get("error", "")
                 if error_msg:
                     print(f"        ERROR: {error_msg}")
 
-    if all_records:
-        df = pd.DataFrame(all_records)
+                # save memory by not tracking records across all trials, just dump them into the csv after each trial
+                cum_df = pd.DataFrame(trial_records)
 
-        # Ensure output directory exists
-        os.makedirs(os.path.dirname(args.output_csv), exist_ok=True)
+                # Ensure output directory exists
+                os.makedirs(os.path.dirname(args.output_csv), exist_ok=True)
+                if (p_idx, algo_idx, trial) != (0, 0, 0):
+                    assert os.path.exists(args.output_csv)
+                    old_df = pd.read_csv(args.output_csv)
+                    cum_df = pd.concat([old_df, cum_df], ignore_index=True)
 
-        df.to_csv(args.output_csv, index=False)
-        print(f"\nResults saved to {args.output_csv}")
+                cum_df.to_csv(args.output_csv, index=False)
+    print(f"\nResults saved to {args.output_csv}")
+    if os.path.exists(args.temp_csv):
+        os.remove(args.temp_csv)
 
 
 if __name__ == "__main__":
