bd7342c60ebe9ead3f674eebaaae7609c2c156b8
dcopper <dcopper@gs25414.sp.cs.cmu.edu>
2026-02-17 11:41:59 -0500

    added arguments to graph_coloring_generator

--- evaluation/graph_coloring_generator.py
+++ evaluation/graph_coloring_generator.py
@@ -1,6 +1,9 @@
-import os
+import os, shutil
 import subprocess
 import sys
+import argparse
+import itertools
+import time
 
 def generate_graph_coloring_problems(
     n_problems: int,
@@ -55,19 +58,35 @@ def generate_graph_coloring_problems(
 
 if __name__ == "__main__":
 
-    INPUT_DIR = "output/graph_coloring_instances_hard"
-    NUM_PROBLEMS = 1
-
+    DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    parser = argparse.ArgumentParser(description="Generate graph coloring instances")
+    parser.add_argument("--output_dir", type=str, default=os.path.join(DIR, "output", "graph_coloring_instances_hard"),
+                        help="directory to save problem .yaml files")
+    parser.add_argument("--num_problems", type=int, default=1, help="Number of graph coloring problems to sample for each case")
+    parser.add_argument("--graph_n",type=int,nargs='+',default=list(range(10,51,10)),
+                        help='number of nodes in the graph to use (can list multiple)')
+    parser.add_argument("--color_count",type=int,nargs='+',default=[3],
+                        help='number of colors available (can list multiple)')
+    parser.add_argument("--dont_use_seed",action='store_true',
+                        help='dont use a random seed (will generate different graphs on each run)')
+    parser.add_argument("--dont_clear_dir",action='store_true',
+                        help='dont clear the directory listed in --output_dir')
+    args = parser.parse_args()
+    if not args.dont_clear_dir and os.path.exists(args.output_dir):
+        for t in range(5,0,-1):
+            print(f"ABOUT TO OVERWRITE {args.output_dir}, if that was not intended, CtrL+C in the next {t} seconds",end='\r')
+            time.sleep(1)
+        shutil.rmtree(args.output_dir)
     # generate random 3 coloring problems
-    for n in range(10, 51, 10):
+    for n,c in itertools.product(args.graph_n,args.color_count):
         generate_graph_coloring_problems(
-            n_problems=NUM_PROBLEMS,
-            output_dir=INPUT_DIR,
+            n_problems=args.num_problems,
+            output_dir=args.output_dir,
             node_count=n,       # -v
-            color_count=3,       # -c
+            color_count=c,       # -c
             graph_type="random", # -g (random, grid, scalefree)
             p_edge=(4.6*n) / (n*(n-1)),           # -p (only used if type is random)
-            use_seed=True,
+            use_seed=not args.dont_use_seed,
         )
 
     print("\nDone.")
