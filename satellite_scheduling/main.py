3cef81d4b4c1f92f0bcc22ef04ff17747d925c27
itai <izilberstein@me.com>
2026-04-03 18:38:14 -0400

    updated satellite_scheduling/main.py to save run data across all listed algorithms

--- satellite_scheduling/main.py
+++ satellite_scheduling/main.py
@@ -1,9 +1,9 @@
-import json,os
-from collections import defaultdict
-from scheduler import solve_local_schedule, get_constraints, add_constraints
-from constraint_generation import solve_constraint_generation
+import json
+import os
+from shortuuid import uuid
 from utils import parse_json_to_dcop_and_overlaps
 from iterative_pricing import solve_iterative_pricing
+from evaluation.algo_configs import get_display_name
 
 MAX_ITERATIONS = 4
 
@@ -11,42 +11,72 @@ MAX_ITERATIONS = 4
 ITAI: something really weird happens where when running this script pydcop runs an entire dcop for a min. Not sure what this is. It happens before any imports or anything...
 """
 
-def main(constraint_generation):
 
-    raw_json = "satellite_scheduling/test_large.json"
-    output_dir="output"
-    os.makedirs(output_dir,exist_ok=True)
-    temp_json = os.path.join( output_dir,"/dcop_global.json")
-    pydcop_results = (output_dir,"/pydcop_results.json")
-    
-    
-    with open("satellite_scheduling/algorithm_configs.json", "r") as f:
-            algorithms = json.load(f)
+def main(
+    scenario,
+    output_json,
+    algorithms_json="satellite_scheduling/algorithm_configs.json",
+):
+    assert not os.path.exists(output_json), f"{output_json} already exists"
+    run_info = {
+        "scenario": scenario,
+        "algorithms_json": algorithms_json,
+    }
+    working_dir = os.path.join("output", "iterative_pricing_runs")
+    os.makedirs(working_dir, exist_ok=True)
+    # temp_json = os.path.join( output_dir,"dcop_global.json")
+    with open(algorithms_json, "r") as f:
+        algorithms = json.load(f)
+    alg_to_info = dict()
+    for algorithm_config in algorithms:
+        algo_name = get_display_name(algorithm_config)
 
+        temp_output_json = os.path.join(working_dir, f"temp_alg_data_{uuid()}.json")
+        print(f"running algorithm {algo_name}, saving to {temp_output_json}")
 
-    algorithm_config = algorithms[0]
-    # Parse JSON
-    (
-        pydcop_dict,
-        agent_tasks,
-        agent_downlinks,
-        agent_capacities,
-        var_to_details,
-        requests,
-    ) = parse_json_to_dcop_and_overlaps(raw_json)
- 
-    best_total_scheduled = solve_iterative_pricing( 
-        pydcop_dict,
-        agent_tasks,
-        agent_downlinks,
-        agent_capacities,
-        var_to_details,
-        requests,
-        algorithm_config,
-        max_iterations = MAX_ITERATIONS
-    )
-    print(best_total_scheduled)
+        # Parse JSON
+        (
+            pydcop_dict,
+            agent_tasks,
+            agent_downlinks,
+            agent_capacities,
+            var_to_details,
+            requests,
+        ) = parse_json_to_dcop_and_overlaps(scenario)
+
+        best_total_scheduled, best_iter, _ = solve_iterative_pricing(
+            pydcop_dict,
+            agent_tasks,
+            agent_downlinks,
+            agent_capacities,
+            var_to_details,
+            requests,
+            algorithm_config,
+            max_iterations=MAX_ITERATIONS,
+            output_json=temp_output_json,
+            working_dir=working_dir,
+            clear_temp_files=True,
+        )
+        print("result", best_total_scheduled)
+        alg_to_info[algo_name] = {"file": temp_output_json, "aux_info": {"best_total_scheduled": best_total_scheduled, "best_iter": best_iter}}
+    # save results
+    output_dic = {"run_info": run_info, "algorithm_configs": algorithms, "output": dict()}
+
+    for algo_name, info in alg_to_info.items():
+        with open(info["file"], "r") as f:
+            data = json.load(f)
+
+        output_dic["output"][algo_name] = {
+            "data": data,
+            "aux_info": info["aux_info"],
+        }
+
+    with open(output_json, "w") as f:
+        json.dump(output_dic, f, indent=2)
+    # clear temp files
+    for _, fn in alg_to_info.items():
+        os.remove(fn)
 
 
 if __name__ == "__main__":
-    main(constraint_generation=True)
+    main(scenario="satellite_scheduling/test.json", output_json="output/test_main.json")
