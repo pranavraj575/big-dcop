fe75d8d3bd5a521079ae3fb55a88a3b6862957d3
pranav <pranavraj575@gmail.com>
2026-04-10 11:59:22 -0400

    better sh script

--- satellite_scheduling/run_experiment.sh
+++ satellite_scheduling/run_experiment.sh
@@ -1,4 +1,5 @@
-cd ~/projects/big-dcop
+BIG_DCOP_DIR="$( dirname -- "$( dirname -- "$( readlink -f -- "${BASH_SOURCE[0]}"; )"; )"; )"
+cd $BIG_DCOP_DIR
 conda activate big-dcop
 
 python satellite_scheduling/main.py \
@@ -9,9 +10,9 @@ python satellite_scheduling/main.py \
 	--timeout 4000
 
 python satellite_scheduling/main.py \
-        --scenario satellite_scheduling/scenarios/scenario_$1.json \
-        --output_json output/sat_sched/output_baseline_scen_$1.json \
-        --algorithms_json satellite_scheduling/baseline_algorithm_configs.json \
-        --pydcop_mode thread \
-        --timeout 4000
+  --scenario satellite_scheduling/scenarios/scenario_$1.json \
+  --output_json output/sat_sched/output_baseline_scen_$1.json \
+  --algorithms_json satellite_scheduling/baseline_algorithm_configs.json \
+  --pydcop_mode thread \
+  --timeout 4000
 
