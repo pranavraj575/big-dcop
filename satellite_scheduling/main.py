35baa74aa357ca30033e8e54b42cb9003586d913
pranav <pranavraj575@gmail.com>
2026-04-20 10:46:10 -0400

    renaming

--- satellite_scheduling/main.py
+++ satellite_scheduling/main.py
@@ -28,7 +28,7 @@ def main(
         "scenario": scenario,
         "algorithms_json": algorithms_json,
     }
-    working_dir = os.path.join("output", "iterative_pricing_runs")
+    working_dir = os.path.join("output", f"{framework}_runs")
     os.makedirs(working_dir, exist_ok=True)
     os.makedirs(os.path.dirname(output_json), exist_ok=True)
     # temp_json = os.path.join( output_dir,"dcop_global.json")
