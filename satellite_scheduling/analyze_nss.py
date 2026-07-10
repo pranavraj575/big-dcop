2668702ec5c5540cd3ccc1a92ed39af92b1e4c56
izilberstein <izilberstein@me.com>
2026-07-10 10:07:09 +0900

    add nss script

---
+++ satellite_scheduling/analyze_nss.py
@@ -0,0 +1,19 @@
+import json
+import numpy as np
+
+with open("satellite_scheduling/scenarios_larger/nss.json", "r") as f: 
+    data = json.load(f)
+
+
+cleaned_data = []
+for item in data:
+    cleaned_item = dict()
+    cleaned_item["percentSatPerIter"] = item["percentSatPerIter"]
+    cleaned_item["messages"] = item["messages"]
+    cleaned_item["percentSatisfied"] = item["percentSatisfied"] 
+    cleaned_item["requestsInCampaign"] = item["requestsInCampaign"]
+    cleaned_item["evaluatedPolicy"] = item["evaluatedPolicy"]
+    cleaned_data.append(cleaned_item)
+
+print(np.mean(cleaned_item["percentSatisfied"]))
+print(np.mean(cleaned_item["messages"]))
