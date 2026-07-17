cfc53dcc22bd5128cdc4f68a1d93dc776c1732eb
pranav <pranavraj575@gmail.com>
2026-07-17 15:27:51 -0400

    plot hyperparam tuning for graph coloring

---
+++ output/make_gc_plots.sh
@@ -0,0 +1,17 @@
+
+# bash graph_coloring/run_experiment.sh --slurm --output-dir output/gc_final --algorithms graph_coloring/configs/algorithm_configs.json --trials 10
+# bash graph_coloring/run_experiment.sh --slurm --output-dir output/gc_final_hyperparams --algorithms graph_coloring/configs/hyperparam_configs.json --trials 10
+
+echo "plotting main results"
+# python graph_coloring/plot_results.py --path output/gc_final/* --algorithms graph_coloring/configs/algorithm_configs_to_plot.json --y_keys cost msg_count msg_count:log --output output/graph_coloring
+
+for algo_path in graph_coloring/configs/hyperparam_configs_*; do
+  nm=$(cut -c 43-100 <<< $algo_path)
+  nm=${nm::-5}
+  echo "plotting" $nm
+  python graph_coloring/plot_results.py \
+    --path output/gc_final_hyperparams/* \
+    --algorithms "${algo_path}" \
+    --y_keys cost msg_count \
+    --output output/gc_plot_hp/"${nm}"&
+done
