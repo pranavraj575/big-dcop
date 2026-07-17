a8a2948d77bf3d5782a85acb1770d6caabb8670f
pranav <pranavraj575@gmail.com>
2026-07-17 17:34:10 -0400

    plotting script

--- output/make_gc_plots.sh
+++ output/make_gc_plots.sh
@@ -1,16 +1,16 @@
 
-# bash graph_coloring/run_experiment.sh --slurm --output-dir output/gc_final --algorithms graph_coloring/configs/algorithm_configs.json --trials 10
-# bash graph_coloring/run_experiment.sh --slurm --output-dir output/gc_final_hyperparams --algorithms graph_coloring/configs/hyperparam_configs.json --trials 10
+# bash graph_coloring/run_experiment.sh --slurm --output-dir output/graph_col_final --algorithms graph_coloring/configs/algorithm_configs.json --trials 10
+# bash graph_coloring/run_experiment.sh --slurm --output-dir output/graph_col_final_hyperparams --algorithms graph_coloring/configs/hyperparam_configs.json --trials 10
 
 for algo_path in graph_coloring/configs/hyperparam_configs_*; do
   nm=${algo_path:42:-5}
   echo "plotting" $nm
   python graph_coloring/plot_results.py \
-    --path output/gc_final_hyperparams/* \
+    --path output/graph_col_final_hyperparams/* \
     --algorithms "${algo_path}" \
     --y_keys cost \
-    --output output/gc_plot_hp/"${nm}";
+    --output output/graph_col_plot_hp/"${nm}";
 done
 
 echo "plotting main results"
-python graph_coloring/plot_results.py --path output/gc_final/* --algorithms graph_coloring/configs/algorithm_configs_to_plot.json --y_keys cost msg_count msg_count:log --output output/graph_coloring
+python graph_coloring/plot_results.py --path output/graph_col_final/* --algorithms graph_coloring/configs/algorithm_configs_to_plot.json --y_keys cost msg_count msg_count:log --output output/graph_coloring
