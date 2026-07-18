31559ff2ebf80e64c807f3b238a5ae827837a1b0
Pranav Rajbhandari <prajbhan@marvel.amem.cs.cmu.edu>
2026-07-17 22:47:07 -0400

    no echo

--- satellite_scheduling/hyperparam_tuning.sh
+++ satellite_scheduling/hyperparam_tuning.sh
@@ -3,7 +3,7 @@ C_VALS=(0 0.01 0.05 0.1 0.15 0.3 0.5 0.75 1 1.25 1.5 1.6 1.7 1.75 1.8 1.9 2 2.25
 
 for C_VAL in "${C_VALS[@]}"; do
   C_STRING="${C_VAL//./_}"
-  echo bash satellite_scheduling/run_experiments.sh \
+  bash satellite_scheduling/run_experiments.sh \
       --no-cg \
       --output-dir output/cosp_final_c_tuning/c_"${C_STRING}" \
       --trials 20 \
