11ea763b062d420e9d8ab2815515ed978502982c
pranav <pranavraj575@gmail.com>
2026-07-20 17:40:11 -0400

    reorder

--- output/make_gc_plots.sh
+++ output/make_gc_plots.sh
@@ -2,6 +2,9 @@
 # bash graph_coloring/run_experiment.sh --slurm --output-dir output/gc_final --algorithms graph_coloring/configs/algorithm_configs.json --trials 10
 # bash graph_coloring/run_experiment.sh --slurm --output-dir output/gc_final_hyperparams --algorithms graph_coloring/configs/hyperparam_configs.json --trials 10
 
+echo "plotting main results"
+python graph_coloring/plot_results.py --path output/gc_final/* --algorithms graph_coloring/configs/algorithm_configs_to_plot.json --y_keys cost msg_count msg_count:log --output output/graph_coloring
+
 for algo_path in graph_coloring/configs/hyperparam_configs_*; do
   nm=${algo_path:42:-5}
   echo "plotting" $nm
@@ -13,6 +16,3 @@ for algo_path in graph_coloring/configs/hyperparam_configs_*; do
   mkdir -p output/gc_hyperparam_optim/"${nm}"
   cp output/gc_plot_hp_/"${nm}"/cost_over_time/rescaled_combined_plot.png output/gc_hyperparam_optim/"${nm}"/rescaled_combined_plot.png
 done
-
-echo "plotting main results"
-python graph_coloring/plot_results.py --path output/gc_final/* --algorithms graph_coloring/configs/algorithm_configs_to_plot.json --y_keys cost msg_count msg_count:log --output output/graph_coloring
