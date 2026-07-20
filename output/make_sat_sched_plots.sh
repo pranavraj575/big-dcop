0aaefe7dc560f0733108f767266d0161a91684ca
pranav <pranavraj575@gmail.com>
2026-07-20 14:55:58 -0400

    made bash nicer

--- output/make_sat_sched_plots.sh
+++ output/make_sat_sched_plots.sh
@@ -1,41 +1,38 @@
 
 # bash satellite_scheduling/run_experiments.sh  --output-dir output/cosp_main --trials 20 --max-iterations 100 --slurm
-# bash satellite_scheduling/run_experiments.sh  --output-dir output/temp_cosp_c_tuning_filt/c_<C VAL STRING> --trials 20 --max-iterations 100 --slurm --step-size-c <C VALUE>
+# bash satellite_scheduling/run_experiments.sh  --output-dir output/cosp_c_tuning/c_<C VAL STRING> --trials 20 --max-iterations 100 --slurm --step-size-c <C VALUE>
 
 MAX_ITER=25
 TEST_MAX_ITER=100
+MAIN_EXP=output/cosp_main
+MAIN_TEMP="${MAIN_EXP}_no_iter_TEMP"
+HP_OPTIM_EXP=output/cosp_c_tuning
+HP_OPTIM_TEMP="${HP_OPTIM_EXP}_TEMP"
 
-rm -r output/temp_cosp_c_tuning_filt/
-rm -r output/temp_cosp_main_no_iterative/
+rm -r ${HP_OPTIM_TEMP}
+rm -r ${MAIN_TEMP}
 
-cp -r output/cosp_main output/temp_cosp_main_no_iterative
-rm -r output/temp_cosp_main_no_iterative/iterative_pricing
-cp -r output/cosp_c_tuning output/temp_cosp_c_tuning_filt
-rm -r output/temp_cosp_c_tuning_filt/c_0 output/temp_cosp_c_tuning_filt/c_100
+cp -r ${MAIN_EXP} ${MAIN_TEMP}
+rm -r "${MAIN_TEMP}/iterative_pricing"
+cp -r ${HP_OPTIM_EXP} ${HP_OPTIM_TEMP}
+rm -r "${HP_OPTIM_TEMP}/c_0" "${HP_OPTIM_TEMP}/c_100"
 
 echo "PLOTTING main experiment"
-python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output output/temp_cosp_c_tuning_filt/* output/temp_cosp_main_no_iterative --plot output/cosp_plots/context --algorithms satellite_scheduling/algo_configs/plot_context_based_algs.json --max-iteration "$MAX_ITER" &
-python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output output/temp_cosp_c_tuning_filt/* output/temp_cosp_main_no_iterative --plot output/cosp_plots/no_context --algorithms satellite_scheduling/algo_configs/plot_default_algs.json --max-iteration "$MAX_ITER" &
-python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output output/temp_cosp_c_tuning_filt/* output/temp_cosp_main_no_iterative --plot output/cosp_plots/all --max-iteration "$MAX_ITER" &
-python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output output/temp_cosp_c_tuning_filt/* output/temp_cosp_main_no_iterative --plot output/cosp_plots/ctx_cmp  --algorithms satellite_scheduling/algo_configs/ctx_comparison.json --max-iteration "$MAX_ITER" &
-python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output output/temp_cosp_c_tuning_filt/* output/temp_cosp_main_no_iterative --plot output/cosp_plots/ONLY_ctx  --algorithms satellite_scheduling/algo_configs/ONLY_ctx_based.json --max-iteration "$MAX_ITER" &
-python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output output/temp_cosp_c_tuning_filt/* output/temp_cosp_main_no_iterative --plot output/cosp_plots/"$TEST_MAX_ITER"_max_iter --algorithms satellite_scheduling/algo_configs/plot_context_based_algs.json --max-iteration "$TEST_MAX_ITER"
+python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output ${HP_OPTIM_TEMP}/* ${MAIN_TEMP} --plot output/cosp_plots/context --algorithms satellite_scheduling/algo_configs/plot_context_based_algs.json --max-iteration "$MAX_ITER" &
+python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output ${HP_OPTIM_TEMP}/* ${MAIN_TEMP} --plot output/cosp_plots/no_context --algorithms satellite_scheduling/algo_configs/plot_default_algs.json --max-iteration "$MAX_ITER" &
+python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output ${HP_OPTIM_TEMP}/* ${MAIN_TEMP} --plot output/cosp_plots/all --max-iteration "$MAX_ITER" &
+python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output ${HP_OPTIM_TEMP}/* ${MAIN_TEMP} --plot output/cosp_plots/ctx_cmp  --algorithms satellite_scheduling/algo_configs/ctx_comparison.json --max-iteration "$MAX_ITER" &
+python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output ${HP_OPTIM_TEMP}/* ${MAIN_TEMP} --plot output/cosp_plots/ONLY_ctx  --algorithms satellite_scheduling/algo_configs/ONLY_ctx_based.json --max-iteration "$MAX_ITER" &
+python satellite_scheduling/analyze_run_experiments.py --hyperparam-optim --output ${HP_OPTIM_TEMP}/* ${MAIN_TEMP} --plot output/cosp_plots/"$TEST_MAX_ITER"_max_iter --algorithms satellite_scheduling/algo_configs/plot_context_based_algs.json --max-iteration "$TEST_MAX_ITER"
 
-
-
-
-#python satellite_scheduling/analyze_run_experiments.py --output output/cosp_final_100_iter --plot output/cosp_solver_plots_final_100_iter/same_scale --max-iteration 100 &
-#python satellite_scheduling/analyze_run_experiments.py --output output/cosp_final_100_iter --plot output/cosp_solver_plots_final_100_iter/diff_scale --not-same --max-iteration 100 &
-#python satellite_scheduling/analyze_run_experiments.py --output output/cosp_final_100_iter --plot output/cosp_solver_plots_final_100_iter/no_context --not-same --max-iteration 100 --algorithms satellite_scheduling/algo_configs/plot_default_algs.json &
-#python satellite_scheduling/analyze_run_experiments.py --output output/cosp_final_100_iter --plot output/cosp_solver_plots_final_100_iter/context --not-same --max-iteration 100 --algorithms satellite_scheduling/algo_configs/plot_context_based_algs.json
 
 echo "PLOTTING all c"
-python satellite_scheduling/analyze_run_experiments.py --output output/temp_cosp_c_tuning_filt/* --plot output/cosp_plots_c_tuning/all_c/no_context --algorithms satellite_scheduling/algo_configs/plot_default_algs.json --max-iteration "$MAX_ITER" &
-python satellite_scheduling/analyze_run_experiments.py --output output/temp_cosp_c_tuning_filt/* --plot output/cosp_plots_c_tuning/all_c/context --algorithms satellite_scheduling/algo_configs/plot_context_based_algs.json --max-iteration "$MAX_ITER"
+python satellite_scheduling/analyze_run_experiments.py --output ${HP_OPTIM_TEMP}/* --plot output/cosp_plots_c_tuning/all_c/no_context --algorithms satellite_scheduling/algo_configs/plot_default_algs.json --max-iteration "$MAX_ITER" &
+python satellite_scheduling/analyze_run_experiments.py --output ${HP_OPTIM_TEMP}/* --plot output/cosp_plots_c_tuning/all_c/context --algorithms satellite_scheduling/algo_configs/plot_context_based_algs.json --max-iteration "$MAX_ITER"
 
 echo "PLOTTING c tuning in parallel"
-for output_path in output/temp_cosp_c_tuning_filt/c_*; do
-  c_var=$(cut -c 34-100 <<< $output_path)
+for output_path in ${HP_OPTIM_TEMP}/c_*; do
+  c_var=${output_path:$(( ${#HP_OPTIM_TEMP} + 3 )):${#output_path}}
   echo "plotting c=${c_var}" &
   python satellite_scheduling/analyze_run_experiments.py --output "${output_path}" --plot output/cosp_plots_c_tuning/c_"${c_var}"/all --max-iteration "$MAX_ITER" &
   python satellite_scheduling/analyze_run_experiments.py --output "${output_path}" --plot output/cosp_plots_c_tuning/c_"${c_var}"/no_context --algorithms satellite_scheduling/algo_configs/plot_default_algs.json --max-iteration "$MAX_ITER" &
